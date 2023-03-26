44cabd1d71934c3dc29bfdfcca55d88c44647571
Joona Nykänen <joona.nykanen@student.lut.fi>
2023-03-26 20:53:01 +0300

    refactor: Deleted additional notes and made code clearer

--- app/src/main/java/com/example/userregistrationapp/UserInputActivity.java
+++ app/src/main/java/com/example/userregistrationapp/UserInputActivity.java
@@ -28,12 +28,22 @@ public class UserInputActivity extends AppCompatActivity {
 
     @SuppressLint("NonConstantResourceId")
     private void addUser() {
-        // Hae käyttäjän syötteet
         EditText firstNameEditText = findViewById(R.id.firstNameInput);
         EditText lastNameEditText = findViewById(R.id.lastNameInput);
         EditText emailEditText = findViewById(R.id.emailInput);
         RadioGroup degreeProgramRadioGroup = findViewById(R.id.degreeProgramRadioGroup);
         RadioGroup imageRadioGroup = findViewById(R.id.imageRadioGroup);
+
+        imageRadioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                RadioButton radioButton1 = findViewById(R.id.imageRadioButton1);
+                RadioButton radioButton2 = findViewById(R.id.imageRadioButton2);
+
+                radioButton1.setSelected(checkedId == R.id.imageRadioButton1);
+                radioButton2.setSelected(checkedId == R.id.imageRadioButton2);
+            }
+        });
 
         String firstName = firstNameEditText.getText().toString();
         String lastName = lastNameEditText.getText().toString();
@@ -52,14 +62,11 @@ public class UserInputActivity extends AppCompatActivity {
             case R.id.imageRadioButton2:
                 imagePath = "image_2";
                 break;
-            // Lisää case-lauseita tarpeen mukaan
         }
 
-        // Luo User-olio ja lisää se UserStorageen
         User newUser = new User(firstName, lastName, email, degreeProgram, imagePath);
         UserStorage.getInstance().addUser(newUser);
 
-        // Vaihda takaisin MainActivityyn
         finish();
     }
 

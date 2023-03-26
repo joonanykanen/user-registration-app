da9d839436f48e7e6b4ce3fe04cbe6d9d3c23b09
Joona Nykänen <joona.nykanen@student.lut.fi>
2023-03-26 18:03:56 +0300

    feat: Added UserListActivity-class

--- app/src/main/java/com/example/userregistrationapp/UserListActivity.java
+++ app/src/main/java/com/example/userregistrationapp/UserListActivity.java
@@ -1,14 +1,25 @@
 package com.example.userregistrationapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class UserListActivity extends AppCompatActivity {
 
+    private RecyclerView userRecyclerView;
+    private UserListAdapter userListAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_list);
+
+        userRecyclerView = findViewById(R.id.userRecyclerView);
+        userListAdapter = new UserListAdapter(UserStorage.getInstance().getUsers());
+        userRecyclerView.setAdapter(userListAdapter);
+        userRecyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
+
 }

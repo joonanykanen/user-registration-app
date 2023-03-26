d739e015ceb79cec910c7ce83024cb8c7ab7ea2d
Joona Nykänen <joona.nykanen@student.lut.fi>
2023-03-26 19:36:36 +0300

    feat: Added selectedImage properties to addUser

--- app/src/main/java/com/example/userregistrationapp/UserInputActivity.java
+++ app/src/main/java/com/example/userregistrationapp/UserInputActivity.java
@@ -2,6 +2,7 @@ package com.example.userregistrationapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -25,12 +26,14 @@ public class UserInputActivity extends AppCompatActivity {
         });
     }
 
+    @SuppressLint("NonConstantResourceId")
     private void addUser() {
         // Hae käyttäjän syötteet
         EditText firstNameEditText = findViewById(R.id.firstNameInput);
         EditText lastNameEditText = findViewById(R.id.lastNameInput);
         EditText emailEditText = findViewById(R.id.emailInput);
         RadioGroup degreeProgramRadioGroup = findViewById(R.id.degreeProgramRadioGroup);
+        RadioGroup imageRadioGroup = findViewById(R.id.imageRadioGroup);
 
         String firstName = firstNameEditText.getText().toString();
         String lastName = lastNameEditText.getText().toString();
@@ -40,8 +43,20 @@ public class UserInputActivity extends AppCompatActivity {
         RadioButton selectedRadioButton = findViewById(selectedRadioButtonId);
         String degreeProgram = selectedRadioButton.getText().toString();
 
+        String imagePath = "";
+        int selectedImageId = imageRadioGroup.getCheckedRadioButtonId();
+        switch (selectedImageId) {
+            case R.id.imageRadioButton1:
+                imagePath = "image_1";
+                break;
+            case R.id.imageRadioButton2:
+                imagePath = "image_2";
+                break;
+            // Lisää case-lauseita tarpeen mukaan
+        }
+
         // Luo User-olio ja lisää se UserStorageen
-        User newUser = new User(firstName, lastName, email, degreeProgram);
+        User newUser = new User(firstName, lastName, email, degreeProgram, imagePath);
         UserStorage.getInstance().addUser(newUser);
 
         // Vaihda takaisin MainActivityyn

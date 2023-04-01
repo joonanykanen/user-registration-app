1cc6bbcaa843b31207e9576060b0496f3be79d7a
Joona Nykänen <joona.nykanen@student.lut.fi>
2023-04-01 14:06:42 +0300

    feat: Added sorting by last name to RecyclerView

--- app/src/main/java/com/example/userregistrationapp/UserListActivity.java
+++ app/src/main/java/com/example/userregistrationapp/UserListActivity.java
@@ -6,6 +6,10 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
 public class UserListActivity extends AppCompatActivity {
 
     private RecyclerView userRecyclerView;
@@ -17,9 +21,17 @@ public class UserListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_list);
         sharedPreferencesManager = new SharedPreferencesManager(this);
+        ArrayList<User> userList = sharedPreferencesManager.loadUserList();
+
+        Collections.sort(userList, new Comparator<User>() {
+            @Override
+            public int compare(User user1, User user2) {
+                return user1.getLastName().compareToIgnoreCase(user2.getLastName());
+            }
+        });
 
         userRecyclerView = findViewById(R.id.userRecyclerView);
-        userListAdapter = new UserListAdapter(sharedPreferencesManager.loadUserList(), this);
+        userListAdapter = new UserListAdapter(userList, this);
         userRecyclerView.setAdapter(userListAdapter);
         userRecyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
